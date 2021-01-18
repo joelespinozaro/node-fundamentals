9aced20fc38212152d3fb179cdb729c6c417943e
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-01-18 13:50:04 -0500

    console methods

---
+++ modulos/consola.js
@@ -0,0 +1,52 @@
+// console.log('Algo');
+
+// console.error('algo pasó');
+
+// console.warn('algo pasó');
+
+// var tabla = [
+//     {
+//         a:1,
+//         b:'z',
+//         c: 'hola'
+//     },
+//     {
+//         a:2,
+//         b:'loquesea'
+//     }
+
+// ]
+// console.log(tabla);
+// console.table(tabla);
+
+
+// console.group('conver');
+// console.log('hola');
+// console.log('bla bla bla');
+// console.log('adios');
+// console.groupEnd('conver');
+
+// function func1() {
+//     console.group('funcion1');
+//     console.log('func1 - primero');
+//     console.log('func1 - segundo');
+//     func2();
+//     console.log('he vuelto al a 1')
+//     console.groupEnd('function1');
+// }
+
+// function func2() {
+//     console.group('funcion2');
+//     console.log('func2 - JAJA');
+//     console.groupEnd('function2');
+// }
+
+// console.log('empezamos');
+// func1();
+
+// console.count('veces');
+// console.count('veces');
+// console.count('veces');
+// console.countReset('veces');
+// console.count('veces');
+// console.count('veces');
