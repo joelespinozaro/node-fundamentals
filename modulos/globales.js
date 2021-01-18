e5cf67517c955a4e412b89e4393671d092235e04
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-01-18 13:33:23 -0500

    var global

---
+++ modulos/globales.js
@@ -0,0 +1,26 @@
+// console.log(global);
+
+// console.log(setInterval);
+// let i = 0;
+// let intervalId = setInterval(function(){
+//     console.log('hola');
+//     if(i === 3) {
+//         clearInterval(intervalId);
+//     }
+//     i++;
+// }, 1000);
+
+
+// setImmediate(()=>{
+//     console.log('inmediato');
+// })
+
+// console.log(process);
+
+// console.log(__dirname);
+
+// console.log(__filename);
+
+// globalThis.miVariable = 'HOLA';
+
+// console.log(miVariable);
