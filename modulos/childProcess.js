611efe72e333eafe867a413dce01343646d9b13c
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-01-18 15:06:25 -0500

    child process

---
+++ modulos/childProcess.js
@@ -0,0 +1,25 @@
+const { exec, spawn } = require('child_process');
+
+// exec('ls -la', (err,stdout, stderr) => {
+//     if(err){
+//         console.error(err);
+//         return false;
+//     }
+
+//     console.log(stdout);
+// })
+
+let proceso = spawn('ls',['-la']);
+console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on('data', function(dato) {
+    console.log('¿Proceso terminado?');
+    console.log(proceso.killed);
+    console.log(dato.toString());
+})
+
+proceso.on('exit', function() {
+    console.log('el proceso terminó');
+    console.log(proceso.killed);
+})
